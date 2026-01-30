932e2452c238a0db6cdc9f42dcd8c168e607e634
zarabby876 <zarabby876@gmail.com>
2026-01-31 01:32:35 +0600

    Update Offbot.js

--- Script/commands/Offbot.js
+++ Script/commands/Offbot.js
@@ -2,13 +2,13 @@ module.exports.config = {
 	name: "offbot",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "🔰𝗦𝗵𝗮𝗸𝗶𝗹_𝗕𝗼𝘁🔰",
 	description: "turn the bot off",
 	commandCategory: "system",
 	cooldowns: 0
         };
 module.exports.run = ({event, api}) =>{
-    const permission = ["100000478146113", "61576357565998"];
+    const permission = ["61584079233024", "100089332602544"];
   	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For ULLASH", event.threadID, event.messageID);
   api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
 }
