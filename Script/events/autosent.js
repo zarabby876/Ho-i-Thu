2f8c50d023dbbe46ba92fa80430d1fd4dc6d1c7c
zarabby876 <zarabby876@gmail.com>
2026-01-31 00:49:42 +0600

    Update autosent.js

--- Script/events/autosent.js
+++ Script/events/autosent.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "autosend",
  eventType: [],
  version: "0.0.1",
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ credits: "‌🅂🄷🄰🄺🄸🄻 🄱🄾🅂🅂",
  description: "Listen events"
 };
 
