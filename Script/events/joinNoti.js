7cb60574d75a6d84f68a201da47366c84897cbcf
zarabby876 <zarabby876@gmail.com>
2026-01-30 18:52:39 +0600

    Update joinNoti.js

--- Script/events/joinNoti.js
+++ Script/events/joinNoti.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "joinNoti",
     eventType: ["log:subscribe"],
     version: "1.0.1",
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "✦─────꯭─⃝‌‌𝐒𝐡𝐚𝐤𝐢𝐥 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭────✦",
     description: "Notification of bots or people entering groups with random gif/photo/video",
     dependencies: {
         "fs-extra": "",
